c76875d40cf148daa8bc11ccc9bf7e7f76871c21
0x0003 <0x0003@users.noreply.github.com>
2026-06-22 05:07:22 +0900

    Add pwsh config

---
+++ home/Documents/WindowsPowerShell/functions/prompt.ps1
@@ -0,0 +1,17 @@
+﻿function global:prompt {
+  $path = switch -Wildcard ($executionContext.SessionState.Path.CurrentLocation.Path) {
+    "$HOME" { "~" }
+    "$HOME\*" { $executionContext.SessionState.Path.CurrentLocation.Path.Replace($HOME, "~") }
+    default { $executionContext.SessionState.Path.CurrentLocation.Path }
+  }
+
+  Set-PSReadLineOption -Colors @{ Command = 'gray' }
+
+  Write-Host " " -NoNewline
+  Write-Host $path -NoNewline -ForegroundColor gray
+  Write-Host " " -NoNewline
+  if ($global:proxy_is_on) { Write-Host "`` " -NoNewline -ForegroundColor yellow }
+  Write-Host "λ." -NoNewline -ForegroundColor darkblue
+  return " "
+}
+
