9fbef6f89fa81c10917c334d395590df3dfbcc13
0x0003 <0x0003@users.noreply.github.com>
2026-09-15 23:16:40 +0900

    pwsh: don't load `set-wallpaper` in elevated prompt

--- home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,8 @@
 #Write-Host 'Powershell' $PsVersionTable.PSVersion '-' (Get-date)
 #Write-Host ''
 
+$isElevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
 # source other parts
 . "$PSScriptRoot\env.ps1"
 . "$PSScriptRoot\functions\prompt.ps1"
@@ -11,7 +13,10 @@
 . "$PSScriptRoot\functions\prx.ps1"
 . "$PSScriptRoot\functions\mpd-kill.ps1"
 . "$PSScriptRoot\functions\split-cuesheet.ps1"
-. "$PSScriptRoot\functions\set-wallpaper.ps1"
+# https://forums.powershell.org/t/powershell-add-type-creates-random-folders-in-temporary-folder/25615
+if (-not $isElevated) {
+    . "$PSScriptRoot\functions\set-wallpaper.ps1"
+}
 
 # readline options
 Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteCharOrExit
